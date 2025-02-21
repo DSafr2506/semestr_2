799c089c82638766aca7b84023e1d1fbb21eac3e
DSafr2506 <saffro@gmail.com>
2025-02-21 14:45:06 +0300

    "Initial commit: CMake project setup".

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int Sum(int a, int b);
+int difference(int k, int l); 
+int multiplication(int h, int j);
+
+int main(){
+
+    std::cout << "Sum: " << Sum(a, b) << std::endl;
+    std::cout << "Difference: " << difference(k, l) << std::endl;
+    std::cout << "Multiplication: " << multiplication(h, j) << std::endl;
+
+    return 0;
+}
+
---
+++ src/utils.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int Sum(int a, int b){
+    int result = a + b;
+    return result ;
+}
+
+int difference(int k, int l){ 
+    int result_02 = l - k;
+    return result_02;
+ 
+}
+int multiplication(int h, int j){
+    int result_03 = h * j; 
+    return result_03;
+}
